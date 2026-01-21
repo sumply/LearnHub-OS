9c78277c33ddb5f3a7f36f16af58696ffec1600b
Vladislav <sumblychannel@gmail.com>
2026-01-21 17:51:33 +0300

    update db

--- database/init.sql
+++ database/init.sql
@@ -1,27 +1,39 @@
-CREATE EXTENSION citext;
+CREATE SCHEMA school;
 
-CREATE SCHEMA IF NOT EXISTS users;
-
-CREATE TYPE users.role AS ENUM('none', 'teacher', 'student');
-CREATE TYPE users.access AS ENUM('user', 'admin');
-
-CREATE DOMAIN users.email AS TEXT CHECK (VALUE ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
--- Секретная информация, используемая при входе в аккаунт.
-CREATE TABLE IF NOT EXISTS users.credentials (
-	id SERIAL PRIMARY KEY,
-	login TEXT NOT NULL UNIQUE,
-	email users.email UNIQUE NOT NULL,
-	password_hash TEXT NOT NULL
+CREATE TABLE school.subjects(
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	name TEXT NOT NULL UNIQUE
 );
 
-CREATE DOMAIN users.name AS VARCHAR(40) CHECK (LENGTH(VALUE) > 2);
--- Общая информация о пользователе.
-CREATE TABLE IF NOT EXISTS users.profiles (
-    id BIGINT REFERENCES users.credentials(id) NOT NULL UNIQUE,
-    first_name users.name NOT NULL,
-    last_name users.name NOT NULL,
-    middle_name VARCHAR(40) NOT NULL,
-    role users.role NOT NULL,
-	access users.access NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE school.groups(
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE SCHEMA account;
+
+CREATE TABLE account.credentials(
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	email TEXT NOT NULL UNIQUE,
+	pwd_hash TEXT NOT NULL
+);
+
+CREATE TABLE account.profiles(
+	id UUID REFERENCES account.credentials(id) NOT NULL UNIQUE,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	role INT NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE account.students(
+	profile_id UUID REFERENCES account.profiles(id) NOT NULL UNIQUE,
+	group_id UUID REFERENCES school.groups(id)
+);
+
+CREATE TABLE account.teachers(
+	profile_id UUID REFERENCES account.profiles(id) NOT NULL,
+	group_id UUID REFERENCES school.groups(id) NOT NULL,
+	subject_id UUID REFERENCES school.subjects(id) NOT NULL,
+	UNIQUE(profile_id, group_id, subject_id)
 );
